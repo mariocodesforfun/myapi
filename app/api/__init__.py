aad2580c879a31228187a339802db3ab6a566abd
mariocodesforfun <gegpriftimario@gmail.com>
2025-04-03 01:28:12 -0400

    test a new route

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,4 +3,6 @@ from flask import Blueprint
 
 example_blueprint = Blueprint("mario_api", __name__)
 
+main_blueprint = Blueprint("mario_api", __name__)
+
 from app.api import routes
--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,6 @@
 
 from flask import jsonify
-from app.api import example_blueprint
+from app.api import example_blueprint, main_blueprint
 
 @example_blueprint.route("/health", methods=["GET"])
 def health():
@@ -8,3 +8,11 @@ def health():
     message = {"message": "API is running, let's start the implementation."}
 
     return jsonify(message), 200
+
+@example_blueprint.route("/", methods=["GET"])
+def main():
+
+    message = {"message": "Nothing to show here, but we're getting there"}
+
+    return jsonify(message), 200
+
